ffcaf9051a868033bbff0d104c91f7e5bd7bf0d0
AngelSony <angelfol@outlook.com>
2023-11-12 16:26:21 -0300

    Create Order Visuals

--- MarketPlace/src/app/components/order-components/create-order/create-order.component.ts
+++ MarketPlace/src/app/components/order-components/create-order/create-order.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ProductService } from 'src/app/services/product-service/product.service';
+import { AuthService } from 'src/app/services/auth-services/auth.service';
+import { Product } from 'src/app/interfaces/product';
 
 @Component({
   selector: 'app-create-order',
   templateUrl: './create-order.component.html',
   styleUrls: ['./create-order.component.css']
 })
-export class CreateOrderComponent {
+export class CreateOrderComponent implements OnInit {
+  productId!: String;
+  productFormEdit!: FormGroup;
+  user!: String;
+  product!: Product
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private authService: AuthService,
+  ) {}
+
+  ngOnInit(){
+    this.user = this.authService.getUser()._id;
+    this.productId = this.route.snapshot.params['id'];
+
+    this.productService
+      .getOneProduct(this.productId)
+      .subscribe((product: any) => {
+        this.product = product
+      });
+  }
 }
